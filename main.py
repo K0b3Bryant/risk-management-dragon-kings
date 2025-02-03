778a16267fdd794d1c69a7b03d3b7f20b7ce75f3
Marco <sejereje1011@gmail.com>
2025-02-03 14:20:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,14 @@ def main():
     bubble_probabilities = np.zeros(len(prices))
     model = create_lstm_model()
     
+    for i in range(50, len(prices)):
+        window_prices = prices.iloc[:i]
+        lppl_params = fit_lppl(window_prices.values.flatten())
+        anomalies = detect_anomalies(window_prices)
+        
+        if lppl_params is not None:
+            bubble_probabilities[i] = anomalies.iloc[-1] * 0.5 + 0.5
+    
     results = pd.DataFrame({"Date": prices.index, "Bubble_Probability": bubble_probabilities})
     results.to_csv(OUTPUT_PATH, index=False)
     
