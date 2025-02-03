787a719bb0c1ccc04f7dbb9d4a039b8b7c431554
Marco <sejereje1011@gmail.com>
2025-02-03 14:18:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
 import numpy as np
 import pandas as pd
 from lppl import fit_lppl
-from anomaly import detect_anomalies
+from anomaly import detect_anomalies, create_lstm_model
 from utils import load_data, plot_results
-from config import DATA_PATH, OUTPUT_PATH, PLOT_RESULTS
+from config import DATA_PATH, OUTPUT_PATH, PLOT_RESULTS, LSTM_PARAMS
 
 def main():
     prices = load_data(DATA_PATH)
     bubble_probabilities = np.zeros(len(prices))
-    
-    for i in range(50, len(prices)):
-        window_prices = prices.iloc[:i]
-        lppl_params = fit_lppl(window_prices.values.flatten())
-        anomalies = detect_anomalies(window_prices)
-        
-        if lppl_params is not None:
-            bubble_probabilities[i] = anomalies.iloc[-1] * 0.5 + 0.5
+    model = create_lstm_model()
     
     results = pd.DataFrame({"Date": prices.index, "Bubble_Probability": bubble_probabilities})
     results.to_csv(OUTPUT_PATH, index=False)
